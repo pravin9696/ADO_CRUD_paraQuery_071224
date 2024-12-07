d05b033bd1807d4aac50d528df20486a4acf3341
Acer <Acer@DESKTOP-ABKHEEV>
2024-12-07 15:40:23 +0530

    Add project files.

---
+++ ADO_CRUD_paraQuery_071224/DB Script/Create dbo.tblStudent.sql
@@ -0,0 +1,17 @@
+﻿USE [myDB123]
+GO
+
+/****** Object: Table [dbo].[tblStudent] Script Date: 7.12.24 15:38:38 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblStudent] (
+    [Roll]        INT          NOT NULL,
+    [Name]        VARCHAR (50) NULL,
+    [total_marks] INT          NULL
+);
+
+
---
+++ ADO_CRUD_paraQuery_071224/Program.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ADO_CRUD_paraQuery_071224
+{
+    class CRU_operation
+    {
+        public void insertRecord_SP()
+        {
+            int rl;
+            string nm;
+            int tm;
+            Console.WriteLine("Enter roll number, Name & Total Marks");
+            rl = int.Parse(Console.ReadLine());
+            nm = Console.ReadLine();
+            tm = int.Parse(Console.ReadLine());
+
+            string qry = "sp_insert";
+
+            SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=myDB123;Integrated Security=True;TrustServerCertificate=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = qry;
+            cmd.CommandType = CommandType.StoredProcedure;
+            //cmd.Parameters.AddWithValue("@nm1", nm);
+            cmd.Parameters.AddWithValue("@rl", rl);
+            cmd.Parameters.AddWithValue("@tm", tm);
+
+            SqlParameter sp1 = new SqlParameter("@nm", nm);
+            cmd.Parameters.Add(sp1);
+            
+
+            int n = cmd.ExecuteNonQuery();
+            if (n > 0)
+            {
+                Console.WriteLine("record inserted Successfully..");
+            }
+            else
+            {
+                Console.WriteLine("Record not inserted!!!");
+            }
+            con.Close();
+
+        }
+        public void insertRecord_para()
+        {
+            int rl;
+            string nm;
+            int tm;
+            Console.WriteLine("Enter roll number, Name & Total Marks");
+            rl = int.Parse(Console.ReadLine());
+            nm = Console.ReadLine();
+            tm = int.Parse(Console.ReadLine());
+
+            string qry = "insert into tblstudent values(@roll,@nm1,@tm)";
+
+            SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=myDB123;Integrated Security=True;TrustServerCertificate=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = qry;
+            //cmd.Parameters.AddWithValue("@nm1", nm);
+            cmd.Parameters.AddWithValue("@roll", rl);           
+            cmd.Parameters.AddWithValue("@tm", tm);
+            
+            SqlParameter sp1=new SqlParameter("@nm1", nm);
+            cmd.Parameters.Add(sp1);
+            cmd.CommandType = CommandType.Text;
+
+            int n = cmd.ExecuteNonQuery();
+            if (n > 0)
+            {
+                Console.WriteLine("record inserted Successfully..");
+            }
+            else
+            {
+                Console.WriteLine("Record not inserted!!!");
+            }
+            con.Close();
+
+        }
+        public void insertRecord()
+        {
+            int rl;
+            string nm;
+            int tm;
+            Console.WriteLine("Enter roll number, Name & Total Marks");
+            rl=int.Parse(Console.ReadLine());
+            nm=Console.ReadLine();
+            tm=int.Parse(Console.ReadLine());
+            string qry = $"insert into tblstudent values({rl},'{nm}',{tm})";
+            SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=myDB123;Integrated Security=True;TrustServerCertificate=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = qry;
+            cmd.CommandType = CommandType.Text;
+
+            int n=cmd.ExecuteNonQuery();
+            if (n>0)
+            {
+                Console.WriteLine("record inserted Successfully..");
+            }
+            else
+            {
+                Console.WriteLine("Record not inserted!!!");
+            }
+            con.Close();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new CRU_operation().insertRecord_SP();
+        }
+    }
+}
